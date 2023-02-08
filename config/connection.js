752c301f65d5475270ab23d4390a88b01aef2ee6
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-02-08 15:07:34 -0800

    final code review and app is ready for deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
-// import sequelize
 const Sequelize = require('sequelize')
-
 require('dotenv').config()
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.DB_PW, {
-      host: 'localhost',
+let sequelize
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: '127.0.0.1', // It always works
       dialect: 'mysql',
       port: 3306,
-    })
+    }
+  )
+}
+
 module.exports = sequelize
